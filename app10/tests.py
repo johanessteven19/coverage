f827dd5746fd19e88ba9ddf06a86d70827e765fe
Johanes Steven <johanessteven19@gmail.com>
2020-05-07 14:19:03 +0700

    [RED] test inside HTML

--- app10/tests.py
+++ app10/tests.py
@@ -5,3 +5,8 @@ class Story10Test(TestCase):
     def test_url_exist(self):
         response = Client().get('')
         self.assertEqual(response.status_code,200)
+    
+    def test_inside_html(self):
+        response=Client().get('')
+        response_content = response.content.decode('utf-8')
+        self.assertIn("test", response_content)
